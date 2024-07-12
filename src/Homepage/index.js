ac5e0362765ae66a564c2b22e67210de35e95265
Agnes <agnesajema@gmail.com>
2024-07-12 07:18:02 +0300

    add files

---
+++ src/Homepage/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './index.css'
+
+const Homepage =()=>{
+ return(
+    <div className='whole'>
+        <div>
+            <h1>OUR TEAM</h1>
+        </div>
+        <div className='images'>
+            <div className='top'>
+        <div className='ivy'>
+           <img src='pics/ivy.png' alt='Ivy'/>
+           <figcaption>Ivy Wanjiku</figcaption>
+           <figcaption>Software Developer</figcaption>
+           <figcaption >Nationality:Kenyan</figcaption>
+           <figcaption className='port'>Portfolio</figcaption>
+           </div>
+           <div className='fanny'>
+           <img src='pics/fanny.png' alt='Fanny'/>
+           <figcaption>Ingabire Fanny</figcaption>
+           <figcaption>Software Developer</figcaption>
+           <figcaption >Nationality:Kenyan</figcaption>
+           <figcaption className='port'>Portfolio</figcaption>
+           </div>
+           </div>
+           <div className='akwang'>
+           <img src='pics/akwang.png' alt='Akwang'/>
+           <figcaption>Akwang Deu</figcaption>
+           <figcaption>Software Developer</figcaption>
+           <figcaption >Nationality:Kenyan</figcaption>
+           <figcaption className='port'>Portfolio</figcaption>
+           </div>
+           <div className='bottom'>
+           <div className='ajema'>
+           <img src='pics/ajema.png' alt='Ajema'/>
+           <figcaption>Ajema Agnes</figcaption>
+           <figcaption>Software Developer</figcaption>
+           <figcaption >Nationality:Kenyan</figcaption>
+           <figcaption className='port'>Portfolio</figcaption>
+           </div>
+           <div className='raziah'>
+           <img src='pics/raziah.png' alt='Raziah'/>
+           <figcaption>Raziah Abdulahi</figcaption>
+           <figcaption>Software Developer</figcaption>
+           <figcaption >Nationality:Kenyan</figcaption>
+           <figcaption className='port'>Portfolio</figcaption>
+           </div>
+           </div>
+        </div>
+                  
+    </div>
+
+ ) ;  
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Homepage
