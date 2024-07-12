ea12900097e643acde3b00f8779c185fd6cce2c1
Raziah <abdullahiraziah@gmail.com>
2024-07-12 07:23:13 +0300

    worked on blog

---
+++ src/Blog/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+
+function Blog(){
+    return(
+
+        <div className='blog-layout-container'>
+    
+     <div className='book-cover'>
+        <img src='images/Born A crime.jpg'></img>
+        </div>
+
+    <div className='aerial-view-book'>
+        <img src='images/aerial view book.jpg'></img>
+    </div>
+
+    <div className='books-and-flowers'>
+        <img src = 'images/books and flowers.jpg'></img>
+
+    </div>
+    {/* <div className='blog-text'>
+        <p>Femme Blogs</p>
+    </div> */}
+   
+        </div>
+
+
+    
+    )
+       
+          
+    }
+export default Blog;
